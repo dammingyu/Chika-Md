3f0e91d6343f7eb0cc52e083f96bc344407170b4
KNETBOT <114360202+dammingyu@users.noreply.github.com>
2023-02-20 20:46:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6289508996342,6282230392514,6287769134132,6289512905644,6285338391264'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6289508996342,6282230392514,6287769134132,6289512905644,6285338391264'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6289508996342,6282230392514,6287769134132,6289512905644,6285338391264' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'ISTP-A MG' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'KNETBOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'KNETBOT SYSTEM' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
+global.youtube = 'https://youtube.com/@knetmusicofficial'
+global.ig = 'https://instagram.com/dammingyu'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.myweb = 'http://www.knetent.my.id'
+global.email = 'knetbot-team@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© KNETBOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'ISTP-A MG' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
